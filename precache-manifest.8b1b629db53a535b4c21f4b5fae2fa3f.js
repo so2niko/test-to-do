9e7999c6a3df3beb39c7ecd2acf97b87574b684e
Nick Sotula <so2niko@gmail.com>
2020-06-11 19:39:28 +0300

    init commit

---
+++ precache-manifest.8b1b629db53a535b4c21f4b5fae2fa3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3348e6b14c41b2958f93bac28a371cc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad3f354e782acaf7282f",
+    "url": "/static/css/main.17c00257.chunk.css"
+  },
+  {
+    "revision": "5ee12485f05289775cf9",
+    "url": "/static/js/2.b9569040.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b9569040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad3f354e782acaf7282f",
+    "url": "/static/js/main.36943c95.chunk.js"
+  },
+  {
+    "revision": "37a0d35451170c581fd7",
+    "url": "/static/js/runtime-main.c1487c63.js"
+  }
+]);
